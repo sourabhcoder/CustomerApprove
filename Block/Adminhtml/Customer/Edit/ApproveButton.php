d801de755a2a8a2065a01655011904b71e21e94c
Sourabh Kumar Sharma <sourabhkumarsharma778@gmail.com>
2017-11-04 18:49:29 +0530

    Update ApproveButton.php

--- Block/Adminhtml/Customer/Edit/ApproveButton.php
+++ Block/Adminhtml/Customer/Edit/ApproveButton.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
+
 namespace Sourabh\CustomerApprove\Block\Adminhtml\Customer\Edit;
 
 use Magento\Customer\Api\AccountManagementInterface;
